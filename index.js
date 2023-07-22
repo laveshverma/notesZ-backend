74ef6a9aeba1d045d571f454101353ff49e8fcb8
LAVESH VERMA <70440150+laveshverma@users.noreply.github.com>
2023-07-22 22:09:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ const port = 5000
 
 app.use(cors())
 app.options('*', cors())
+app.use(function(req, res, next) {
+     res.header("Access-Control-Allow-Origin", "*");
+     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+     next();
+});
 app.use(express.json())
 // Available Routes
 app.use('/api/auth',require('./routes/auth'))
